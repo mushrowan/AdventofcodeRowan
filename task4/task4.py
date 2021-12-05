8227c0547e7f8c40f8cdc750f9feda0f372b6379
Rowan Amber Jones <rowamber@outlook.com>
2021-12-05 02:05:35 +0000

    more task 4 stuff

---
+++ task4/task4.py
@@ -0,0 +1,3 @@
+#bingo: which will win first?
+#beginning of round: take number, fill in grid (probably with a list pair for each number saying whether it is filled in or not)
+#end of round: check, has anybody won?
