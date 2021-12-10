5146ed068ae5be583a72cea88fb639b40c21e862
Rowan Amber Jones <rowamber@outlook.com>
2021-12-10 22:00:19 +0000

    task 5 setup

---
+++ task5/task5.py
@@ -0,0 +1,8 @@
+#ideas:
+""" 
+1. format the items in a way such that we have a list of lines: [[x1,y1][x2,y2]] and a list of these. we can use a range[x:y+1] (+1 so we incluse y itself) to create lists of points. then we can create a list of intersections, and figure it out from there.
+
+
+
+
+"""
